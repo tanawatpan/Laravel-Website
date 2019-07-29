9fe641ab235912d58e42d7837eb503e67cf49c6b
TheKOOPT <52491236+TheKOOPT@users.noreply.github.com>
2019-07-29 23:34:42 +0700

    Revert "a link test"
    This reverts commit 8cd778e16308ff2c95a2395b5729f6ee50695428.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="card-body">
                 <div class="row justify-content-center">
                     <i class="px-3 py-2 fa fa-facebook-official fa-2x" aria-hidden="true"></i>
-                    <a href="https://www.w3schools.com"><i class="px-3 py-2 fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
+                    <i class="px-3 py-2 fa fa-twitter-square fa-2x" aria-hidden="true"></i>
                     <i class="px-3 py-2 fa fa-tumblr-square fa-2x" aria-hidden="true"></i>
                     <i class="px-3 py-2 fa fa-instagram fa-2x" aria-hidden="true"></i>
                 </div>
