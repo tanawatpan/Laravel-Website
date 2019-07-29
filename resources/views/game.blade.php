6aca1b17c5d19c5ec214a7de908e7c45109031da
TheKOOPT <52491236+TheKOOPT@users.noreply.github.com>
2019-07-29 18:00:45 +0700

    test3

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -6,7 +6,7 @@
         <h1 class="display-4">Game</h1>
     </div>
 
-    <div class = "md-12 my-2">
+    <div class = "col-md-12 my-2">
         <img src="/storage/We_are_Escape.jpg">
     </div>
     
