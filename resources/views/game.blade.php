19428e4e098f71af6444eb6f9142cb4b1d857d8d
TheKOOPT <52491236+TheKOOPT@users.noreply.github.com>
2019-07-29 18:17:09 +0700

    center-block

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -6,8 +6,8 @@
         <h1 class="display-4">Game</h1>
     </div>
 
-    <div class="col-md-8 col-md-offset-5 justify-content-center">
-        <img src="/storage/We_are_Escape.jpg" class="img-fluid img-thumbnail justify-content-center" alt="Responsive image"
+    <div class="col-md-8 col-md-offset-5 justify-content-center center-block">
+        <img src="/storage/We_are_Escape.jpg" class="img-fluid img-thumbnail justify-content-center center-block" alt="Responsive image"
             style="width: 100%;height: auto;">
     </div>
     
