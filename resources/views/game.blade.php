17ef57cfd887e6e81b2c9d14333a8d30f25fde12
TheKOOPT <52491236+TheKOOPT@users.noreply.github.com>
2019-07-29 18:03:57 +0700

    test4

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -9,6 +9,11 @@
     <div class = "col-md-12 my-2">
         <img src="/storage/We_are_Escape.jpg">
     </div>
+
+    <div class="col-md-12 col-md-offset-5">
+        <img src="/storage/We_are_Escape.jpg" class="img-fluid img-thumbnail" alt="Responsive image"
+            style="width: 100%;height: auto;">
+    </div>
     
 
 @endsection
