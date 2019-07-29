6bdccc8a21d1a820a3c7f1f43121a316f56766d9
TheKOOPT <52491236+TheKOOPT@users.noreply.github.com>
2019-07-29 17:43:57 +0700

    test commit

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -4,6 +4,6 @@
 
     <div class="jumbotron text-white text-center font-weight-bold" style = "background-color:#56b6c5;">
         <h1 class="display-4">Game</h1>
-    </div>
+    </div>gbhnyujk
 
 @endsection
