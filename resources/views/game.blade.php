8fac0cb13545bef43b4a3613dd286a215cb1b8a9
TheKOOPT <52491236+TheKOOPT@users.noreply.github.com>
2019-07-29 18:19:54 +0700

    2-8-2 image

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -6,10 +6,20 @@
         <h1 class="display-4">Game</h1>
     </div>
 
-    <div class="col-md-8 col-md-offset-5 justify-content-center center-block">
-        <img src="/storage/We_are_Escape.jpg" class="img-fluid img-thumbnail justify-content-center center-block" alt="Responsive image"
-            style="width: 100%;height: auto;">
+    <div class = 'row'>
+
+        <div class = 'col-md2'></div>
+
+        <div class="col-md-8 col-md-offset-5">
+            <img src="/storage/We_are_Escape.jpg" class="img-fluid img-thumbnail" alt="Responsive image"
+                style="width: 100%;height: auto;">
+        </div>
+
+        <div class = 'col-md2'></div>
+
     </div>
+
+
     
 
 @endsection
