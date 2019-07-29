23452e910a2f1d8b958de3f4b43fceb88c23b537
TheKOOPT <52491236+TheKOOPT@users.noreply.github.com>
2019-07-29 17:58:38 +0700

    test2

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -5,6 +5,10 @@
     <div class="jumbotron text-white text-center font-weight-bold" style = "background-color:#56b6c5;">
         <h1 class="display-4">Game</h1>
     </div>
-    <img class="md-12 my-2" src="/storage/We_are_Escape.jpg">
+
+    <div class = "md-12 my-2">
+        <img src="/storage/We_are_Escape.jpg">
+    </div>
+    
 
 @endsection
