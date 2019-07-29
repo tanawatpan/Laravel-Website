1110dd4b456ae888c1f2948a379ee617346c374c
TheKOOPT <52491236+TheKOOPT@users.noreply.github.com>
2019-07-29 18:11:30 +0700

    TEST 7

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -5,8 +5,8 @@
     <div class="jumbotron text-white text-center font-weight-bold" style = "background-color:#56b6c5;">
         <h1 class="display-4">Game</h1>
     </div>
-    
-    <div class="col-md-12 col-md-offset-5 my-2">
+
+    <div class="col-md-8 col-md-offset-5">
         <img src="/storage/We_are_Escape.jpg" class="img-fluid img-thumbnail" alt="Responsive image"
             style="width: 100%;height: auto;">
     </div>
