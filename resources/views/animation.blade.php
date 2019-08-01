2288c0d7c9a9dca0c182d57e1a233b043c1031e8
TheKOOPT <52491236+TheKOOPT@users.noreply.github.com>
2019-08-02 00:13:07 +0700

    upload pics

--- resources/views/animation.blade.php
+++ resources/views/animation.blade.php
@@ -6,4 +6,15 @@
         <h1 class="display-4">Animation</h1>
     </div>
 
+    <div class = 'row'>
+
+        <div class = 'col-md-2'></div>
+
+        <div class="col-md-8 col-md-offset-5">
+            <img src="/storage/Lyn's art problem underconstruction.jpg" class="img-fluid img-thumbnail" alt="Responsive image"
+        style="width: 100%;height: auto;">
+        </div>
+
+        <div class = 'col-md-2'></div>
+    </div>
 @endsection
--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -5,5 +5,15 @@
     <div class="jumbotron text-white text-center font-weight-bold" style = "background-color:#f3ce5f;">
         <h1 class="display-4">Commission Info.</h1>
     </div>
+    <div class = 'row'>
 
+        <div class = 'col-md-2'></div>
+
+        <div class="col-md-8 col-md-offset-5">
+            <img src="/storage/Comm table 2019" class="img-fluid img-thumbnail" alt="Responsive image"
+            style="width: 100%;height: auto;">
+        </div>
+
+    <div class = 'col-md-2'></div>
+</div>
 @endsection
